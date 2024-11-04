6c5de1778391d27c1db0103ebf8c7a596233a887
Kasidit <Sobingeneral@gmail.com>
2024-11-05 00:35:21 +0700

    feat: update colorscheme

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -1,46 +1,16 @@
 return {
-	-- "sainnhe/everforest",
-	-- "shaunsingh/solarized.nvim",
-	-- "Tsuzat/NeoSolarized.nvim",
-	-- "ellisonleao/gruvbox.nvim",
-	-- "p00f/alabaster.nvim",
-	"ishan9299/nvim-solarized-lua",
-	-- "aktersnurra/no-clown-fiesta.nvim",
-	-- "NTBBloodbath/doom-one.nvim",
+	"maxmx03/solarized.nvim",
 	lazy = false,
 	priority = 1000,
 
-	-- config = function()
-	-- 	-- vim.cmd.colorscheme("everforest")
-	-- 	-- vim.cmd.colorscheme("alabaster")
-	-- 	-- vim.cmd([[ colorscheme NeoSolarized ]])
-	--
-	-- 	-- vim.g.solarized_italics = 0
-	-- 	-- vim.cmd.colorscheme("solarized")
-	-- 	-- require("no-clown-fiesta").setup({
-	-- 	-- require("doom-one").setup({
-	-- 	-- 	transparent = false, -- Enable this to disable the bg color
-	-- 	-- 	styles = {
-	-- 	-- 		-- You can set any of the style values specified for `:h nvim_set_hl`
-	-- 	-- 		comments = {},
-	-- 	-- 		functions = {},
-	-- 	-- 		keywords = {},
-	-- 	-- 		lsp = { underline = true },
-	-- 	-- 		match_paren = {},
-	-- 	-- 		type = { bold = false },
-	-- 	-- 		variables = {},
-	-- 	-- 	},
-	-- 	-- })
-	-- 	-- require("doom-one").setup({})
-	--
-	-- 	-- vim.cmd([[colorscheme no-clown-fiesta]])
-	-- 	vim.cmd([[colorscheme doom-one]])
-	-- end,
-	-- config = function()
-	-- 	vim.cmd.colorscheme("doom-one")
-	-- end,
 	config = function()
-		vim.g.solarized_italics = 0
+		vim.o.termguicolors = true
+		vim.o.background = "dark"
+		require("solarized").setup({
+			styles = {
+				enabled = false,
+			},
+		})
 		vim.cmd.colorscheme("solarized")
 	end,
 }
