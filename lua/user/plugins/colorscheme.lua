d451ecfb38cd18d46d614db600d2b5c5caeb24ec
Kasidit <Sobingeneral@gmail.com>
2024-12-03 00:56:07 +0700

    feat: update color scheme

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -1,16 +1,28 @@
 return {
-	"maxmx03/solarized.nvim",
+	-- "maxmx03/solarized.nvim",
+	"neanias/everforest-nvim",
 	lazy = false,
 	priority = 1000,
-
+	-- config = function()
+	-- 	vim.o.termguicolors = true
+	-- 	vim.o.background = "dark"
+	-- 	require("solarized").setup({
+	-- 		styles = {
+	-- 			enabled = false,
+	-- 		},
+	-- 	})
+	-- 	vim.cmd.colorscheme("solarized")
+	-- end,
 	config = function()
 		vim.o.termguicolors = true
 		vim.o.background = "dark"
-		require("solarized").setup({
-			styles = {
-				enabled = false,
-			},
+		require("everforest").setup({
+			background = "hard",
+			diagnostic_virtual_text = "coloured",
+			diagnostic_text_highlight = true,
+			italics = false,
+			disable_italic_comments = true,
 		})
-		vim.cmd.colorscheme("solarized")
+		vim.cmd.colorscheme("everforest")
 	end,
 }
