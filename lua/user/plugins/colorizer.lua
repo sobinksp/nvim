71090150a059683e935fe02c6fcce454a6a8287a
Kasidit <Sobingeneral@gmail.com>
2025-01-13 12:19:42 +0700

    fix: fix colorizer doesn't highlight properly

--- lua/user/plugins/colorizer.lua
+++ lua/user/plugins/colorizer.lua
@@ -2,8 +2,6 @@ return {
 	"norcalli/nvim-colorizer.lua",
 	config = function()
 		local colorizer = require("colorizer")
-		colorizer.setup({
-			names = false,
-		})
+		colorizer.setup({ "*" }, { names = false })
 	end,
 }
