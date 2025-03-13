a30c3f9a4d93cc0d501e43d3c63911f194291ce7
Kasidit <kasidit.cha@easyrice.ai>
2025-03-13 23:31:35 +0700

    feat: remove todo

--- lua/user/plugins/todo-comments.lua
+++ lua/user/plugins/todo-comments.lua
@@ -1,17 +1,19 @@
-return {
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    -- FIXME: this is a test
-    signs = false,
-    highlight = {
-      multiline = false,
-      keyword = "fg",
-      after = ""
-    },
-    gui_style = {
-      fg = "NONE", -- The gui style to use for the fg highlight group.
-      bg = "NONE", -- The gui style to use for the bg highlight group.
-    },
-  }
-}
+-- return {
+-- 	"folke/todo-comments.nvim",
+-- 	enabled = false,
+-- 	dependencies = { "nvim-lua/plenary.nvim" },
+-- 	opts = {
+-- 		-- FIXME: this is a test
+-- 		signs = false,
+-- 		highlight = {
+-- 			multiline = false,
+-- 			keyword = "fg",
+-- 			after = "",
+-- 		},
+-- 		gui_style = {
+-- 			fg = "NONE", -- The gui style to use for the fg highlight group.
+-- 			bg = "NONE", -- The gui style to use for the bg highlight group.
+-- 		},
+-- 	},
+-- }
+return {}
